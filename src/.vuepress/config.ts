bfa4671ce506e8fb56826758963db91e767e1eb1
Mr.Blue <silverhugh.77@gmail.com>
2023-01-26 15:38:03 +0800

    deploy

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,23 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/",
+
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "DeepCodebase",
+      description: "A template for deep learning projects.",
+    },
+    "/zh/": {
+      lang: "zh-CN",
+      title: "DeepCodebase",
+      description: "深度学习项目模板。",
+    },
+  },
+
+  theme,
+
+  shouldPrefetch: false,
+});
